b2e9d5ad6e31b643763da0997fc3c1d4ffad7317
Leandro Safra <59981353+SafraPC@users.noreply.github.com>
2021-04-20 09:02:39 -0300

    Put elements in screen

---
+++ mockArrayFilter/mock.js
@@ -0,0 +1,62 @@
+//create a mock data
+
+const mockData = [
+    {
+      label: "Leandro",
+      age: 18,
+    },
+    {
+      label: "Beatriz",
+      age: 15,
+    },
+    {
+      label: "Junior",
+      age: 17,
+    },
+    {
+      label: "Felipe",
+      age: 25,
+    },
+    {
+      label: "Larissa",
+      age: 30,
+    },
+    {
+      label: "Thiago",
+      age: 33,
+    },
+    {
+      label: "Gabriel",
+      age: 12,
+    },
+    {
+      label: "Lucas",
+      age: 11,
+    },
+    {
+      label: "Beatriz",
+      age: 15,
+    },
+    {
+      label: "Felipe",
+      age: 18,
+    },
+    {
+      label: "Larissa",
+      age: 18,
+    },
+  ];
+  
+const myNames = document.getElementById('names')
+const myAges = document.getElementById('ages')
+
+mockData.forEach(item=>{
+    const names = document.createElement('p');
+    names.innerText = item.label;
+    myNames.appendChild(names);
+})
+mockData.forEach(item=>{
+    const ages = document.createElement('p');
+    ages.innerText = item.age;
+    myAges.appendChild(ages);
+})

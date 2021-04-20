af1250f83947f3cb404af64b4335e5b94e9db656
Leandro Safra <59981353+SafraPC@users.noreply.github.com>
2021-04-20 17:36:25 -0300

    create new sections for adapted content

---
+++ mockArrayFilter/scripts.js
@@ -0,0 +1,9 @@
+//This file is only for validations and other, is only for mock the data and put one in view.
+
+//get mock data from html
+
+const allNames = document.getElementById('names');
+const allAges = document.getElementById('ages');
+
+console.log(allNames);
+console.log(allAges)

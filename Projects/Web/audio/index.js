d0039ac1be0d11bd57e9a8f50b086fd1240b2f69
Carradori <felipe_carradori@hotmail.com>
2021-06-30 12:22:50 -0300

    fix: many audios playing

--- Projects/Web/audio/index.js
+++ Projects/Web/audio/index.js
@@ -18,10 +18,29 @@ class MyMediaRecorder {
 
 		navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
 			this.mediaRecorder = new MediaRecorder(stream);
-			this.audioChunks = [];
-
 			this.mediaRecorder.addEventListener("dataavailable", (event) => {
+				this.audioChunks = [];
 				this.audioChunks.push(event.data);
+			});
+			this.mediaRecorder.addEventListener("stop", (e) => {
+				if (this.isCancel) {
+					this.audioChunks = [];
+					return;
+				}
+
+				const audioBlob = new Blob(this.audioChunks, {
+					type: "audio/mp3; code=opus",
+				});
+				const reader = new window.FileReader();
+				reader.readAsDataURL(audioBlob);
+
+				reader.addEventListener("loadend", (res) => {
+					console.log(res.target.result);
+				});
+				const audioUrl = URL.createObjectURL(audioBlob);
+				const audio = new Audio(audioUrl);
+				audio.play();
+				this.audioChunks = [];
 			});
 		});
 	}
@@ -30,34 +49,12 @@ class MyMediaRecorder {
 		if (this.isMediaRecording) return;
 		this.audioChunks = [];
 		this.isCancel = false;
-		this.mediaRecorder.start();
 		this.isMediaRecording = true;
+		this.mediaRecorder.start();
 	};
 
 	stop = () => {
 		if (this.mediaRecorder.state === "inactive") return;
-		this.mediaRecorder.addEventListener("stop", () => {
-			if (this.isCancel || this.isMediaRecording) {
-				this.audioChunks = [];
-				this.isMediaRecording = false;
-				return;
-			}
-
-			const audioBlob = new Blob(this.audioChunks, {
-				type: "audio/mp3; code=opus",
-			});
-			const reader = new window.FileReader();
-			reader.readAsDataURL(audioBlob);
-
-			reader.addEventListener("loadend", (res) => {
-				// console.log(res.target.result);
-			});
-
-			const audioUrl = URL.createObjectURL(audioBlob);
-			const audio = new Audio(audioUrl);
-			audio.play();
-		});
-
 		this.isMediaRecording = false;
 		this.mediaRecorder.stop();
 	};

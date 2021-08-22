314fe11c81a290a44afe69d995c71e6f3d063878
safrapc <leandrosafra.jconectada@gmail.com>
2021-08-22 12:37:43 -0300

    can get cep

--- Web/complete_website/getCEP/index.js
+++ Web/complete_website/getCEP/index.js
@@ -1,6 +1,7 @@
-let searchButton = document.getElementById("searchButton");
-let inputCEP = document.getElementById("cepInput");
-let form = document.getElementById("cepForm");
+//declare variables
+const searchButton = document.getElementById("searchButton");
+const inputCEP = document.getElementById("cepInput");
+const form = document.getElementById("cepForm");
 //cancel form refresh
 const handleForm = (event) => {
   event.preventDefault();
@@ -9,6 +10,33 @@ form.addEventListener("submit", handleForm);
 
 //Get values from inputCEP
 
-const getCEP = () => {};
+const getCEP = async () => {
+  try {
+    const data = await fetch(
+      `https://viacep.com.br/ws/${inputCEP.value}/json/`,
+      {
+        method: "GET",
+        mode: "cors",
+        cache: "default",
+      }
+    );
+    if (data.status === 200) {
+      const respData = await data.json();
+      console.log(respData);
+      return;
+    }
+    if (data.status === 400) {
+      console.log("CEP Inválido!!");
+      return;
+    }
+    if (data.status === 500) {
+      console.log("Servidor em manutenção");
+      return;
+    }
+  } catch (error) {
+    console.log("CEP Request Error =>", error);
+    return;
+  }
+};
 
-searchButton.onclick = getCEP();
+searchButton.onclick = getCEP;
